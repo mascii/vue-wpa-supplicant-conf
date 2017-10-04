801dd1e9ec8679a7cae82d45a4c2830b934b8301
Masaki Koyanagi <mascii@gmail.com>
2017-10-04 22:07:24 +0900

    pluginをwebpack.DefinePluginに変更

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
     }
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
   ],
   module: {
     loaders: [
