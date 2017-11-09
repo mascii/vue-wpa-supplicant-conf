9095640851a5645e18cb9e140e2fb0e01368799f
Masaki Koyanagi <mascii@gmail.com>
2017-11-09 23:44:23 +0900

    inline-sourceのcompress設定を無効化

--- inline.js
+++ inline.js
@@ -1,5 +1,5 @@
 const inline = require('inline-source').sync;
 const fs = require('fs');
 
-const html = inline('./index.html');
+const html = inline('./index.html', { compress: false });
 fs.writeFileSync('./dist/index.html', html);
